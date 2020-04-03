46a7d29ab5c8ea25b13bb7499bf63771088e23bc
David - Inaudit <david@inaudit.es>
2020-04-03 21:09:32 +0200

    First commit.

---
+++ backend/routes/bulbs.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+var TPLSmartDevice = require('tplink-lightbulb');
+const path = require('path');
+
+/* GET bulb page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express bulb work fine!' });
+  });
+  
+/* GET turn on/off light bulb. */
+router.get('/state/:num', function(req, res, next) {
+    const num = Number(req.params.num);
+    const light = new TPLSmartDevice('192.168.1.84')
+    light.send({
+        'smartlife.iot.smartbulb.lightingservice': {
+            'transition_light_state': {
+            'on_off': num,
+            'transition_period': 0
+            }
+        }
+    })
+    .then(response => {
+        res.send(response)
+    })
+    .catch(e => console.error(e))
+});
+
+module.exports = router;
