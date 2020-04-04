7e1b0798dc942e4ca442014bb60ef9ef0c85956c
David - Inaudit <david@inaudit.es>
2020-04-04 15:55:31 +0200

    Added helper bulbs.

---
+++ backend/helpers/bulbs.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var TPLSmartDevice = require('tplink-lightbulb');
+
+function stateBulbs(state, callback) {
+    const light = new TPLSmartDevice('192.168.1.84')
+    light.send({
+        'smartlife.iot.smartbulb.lightingservice': {
+            'transition_light_state': {
+                'on_off': state,
+                'transition_period': 0
+            }
+        }
+    })
+        .then(response => {
+            return callback(response);
+        })
+        .catch(e => console.error(e))
+}
+
+module.exports = stateBulbs;
--- backend/routes/assistant.js
+++ backend/routes/assistant.js
@@ -3,9 +3,15 @@ var router = express.Router();
 
 const path = require('path');
 const GoogleAssistant = require('google-assistant');
-const readline = require('readline');
 
-router.get('/ask', function(req, res, next) {
+var Bulbs = require('../helpers/bulbs');
+
+router.get('/ask', function (req, res, next) {
+    const ask = req.query.ask;
+    if (ask == 'adios') next('route');
+    else next();
+}, function (req, res, next) {
+    const query = req.query.ask;
     const config = {
         auth: {
             keyFilePath: path.resolve(__dirname, '../google-home-credentials/auth.json'),
@@ -16,59 +22,56 @@ router.get('/ask', function(req, res, next) {
             lang: 'es-ES', // defaults to en-US, but try other ones, it's fun!
             showDebugInfo: false, // default is false, bug good for testing AoG things
         },
-      };
+    };
 
-      const startConversation = (conversation) => {
+    const startConversation = (conversation) => {
         // setup the conversation
         conversation
-          .on('response', text =>{
-              res.send(text)
-            console.log('Assistant Response:', text)
-          })
-          .on('debug-info', info => console.log('Debug Info:', info))
-          // if we've requested a volume level change, get the percentage of the new level
-          .on('volume-percent', percent => console.log('New Volume Percent:', percent))
-          // the device needs to complete an action
-          .on('device-action', action => console.log('Device Action:', action))
-          // once the conversation is ended, see if we need to follow up
-          .on('ended', (error, continueConversation) => {
-            if (error) {
-              console.log('Conversation Ended Error:', error);
-            } else if (continueConversation) {
-              promptForInput();
-            } else {
-              console.log('Conversation Complete');
-              conversation.end();
-            }
-          })
-          // catch any errors
-          .on('error', (error) => {
-            console.log('Conversation Error:', error);
-          });
-      };
-      
-      const promptForInput = () => {
-        // type what you want to ask the assistant
-        const rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout,
-        });
-      
-        rl.question('Type your request: ', (request) => {
-          // start the conversation
-          config.conversation.textQuery = request;
-          assistant.start(config.conversation, startConversation);
-      
-          rl.close();
-        });
-      };
-      
+            .on('response', text => {
+                res.send(text)
+                //console.log('Assistant Response:', text)
+            })
+            .on('debug-info', info => console.log('Debug Info:', info))
+            // if we've requested a volume level change, get the percentage of the new level
+            .on('volume-percent', percent => console.log('New Volume Percent:', percent))
+            // the device needs to complete an action
+            .on('device-action', action => console.log('Device Action:', action))
+            // once the conversation is ended, see if we need to follow up
+            .on('ended', (error, continueConversation) => {
+                if (error) {
+                    console.log('Conversation Ended Error:', error);
+                } else if (continueConversation) {
+                    promptForInput();
+                } else {
+                    console.log('Conversation Complete');
+                    conversation.end();
+                }
+            })
+            // catch any errors
+            .on('error', (error) => {
+                console.log('Conversation Error:', error);
+            });
+    };
+
+    const promptForInput = () => {
+        // Iniciar la conversacion con la pregunta que le emos pasado desde la url
+        config.conversation.textQuery = query;
+        assistant.start(config.conversation, startConversation);
+    };
+
     const assistant = new GoogleAssistant(config.auth);
-        assistant
-          .on('ready', promptForInput)
-          .on('error', (error) => {
+    assistant
+        .on('ready', promptForInput)
+        .on('error', (error) => {
             console.log('Assistant Error:', error);
         });
+});
+
+router.get('/ask', function (req, res, next) {
+    const state = Number(req.query.state);
+    const bulbs = new Bulbs(state, function (response) {
+        res.send(response)
+    });
 });
 
 module.exports = router;
