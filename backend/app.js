7759ef6a74b736f63e26eb73b835c48c61ce7b96
David - Inaudit <david@inaudit.es>
2020-04-05 01:20:00 +0200

    Added libnmap

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bulbsRouter = require('./routes/bulbs');
 var assistantRouter = require('./routes/assistant');
+var libnmapRouter = require('./routes/libnmap');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bulbs', bulbsRouter);
 app.use('/assistant', assistantRouter);
+app.use('/libnmap', libnmapRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
